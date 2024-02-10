0dacb51b3b6d49914204670fee581cf93331fc4e
gagan <gasingla07@gmail.com>
2024-02-10 08:00:02 -0600

    read method

--- read_poem.py
+++ read_poem.py
@@ -16,10 +16,11 @@
 with open('D:/Python/jabberwocky.txt', 'r') as jabber:
     # for line in jabber:
     #     print(line.rstrip())
-    lines = jabber.readlines()
+    lines = jabber.read(100)
+    print(len(lines))
 
 
-print(lines)
-print(lines[-1:])
-for line in reversed(lines):
-    print(line, end='--')
+# print(lines)
+# print(lines[-1:])
+# for line in reversed(lines):
+#     print(line, end='--')
