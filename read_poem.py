bd888bc479b0a570efd96b4b188fd84ea855954d
gagan <gasingla07@gmail.com>
2024-02-10 07:51:39 -0600

    readlines methods

--- read_poem.py
+++ read_poem.py
@@ -9,5 +9,17 @@
 # jabber.close()
 
 
-newstring = "       abcsssssabc    "
-print(newstring.lstrip(' abc'))
+# newstring = "       abcsssssabc    "
+# print(newstring.lstrip(' abc'))
+
+
+with open('D:/Python/jabberwocky.txt', 'r') as jabber:
+    # for line in jabber:
+    #     print(line.rstrip())
+    lines = jabber.readlines()
+
+
+print(lines)
+print(lines[-1:])
+for line in reversed(lines):
+    print(line, end='--')
