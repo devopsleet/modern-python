3a225e8faf40578fc24f8a269ae6de3206f414b4
gagan <gasingla07@gmail.com>
2024-08-16 09:34:38 -0500

    Hashmap

--- rbr.py
+++ rbr.py
@@ -1,14 +1,16 @@
-from collections import defaultdict
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        result = []
+        n = max(len(word1), len(word2))
+        for i in range(n):
+            if i < len(word1):
+                result.extend(word1[i])
+                print(result)
+            if i < len(word2):
+                result += word2[i]
 
-text = "hello world"
-char_count = defaultdict(int)
-
-print(char_count)
-
-for char in text:
-    char_count[char] += 1
-
-#print(char_count)
+        return "".join(result)
 
 
-build = "gagan"
+S1 = Solution()
+print(S1.mergeAlternately("abc", "def"))
