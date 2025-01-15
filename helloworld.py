fe816ca6e142bddea35df45c751d5e6bb3ac2a88
gagan <gasingla07@gmail.com>
2025-01-15 09:47:08 -0600

    nested while loop

--- helloworld.py
+++ helloworld.py
@@ -1,24 +1,11 @@
-election_parties = ["bjp", "congress", "dmk", "x"]
+# While loop
 
-constituency = [240, 100, 40]
+i = 10
+j = 5
 
-# bjp 240
-# congress 100
-# dmk 40
-
-for party in election_parties:
-    print(party)
-
-for value in constituency:
-    print(value)
-
-
-for party, value in zip(election_parties, constituency):
-    print(party, value)
-
-
-tuple1 = ('p', 'q')
-set1 = {100, 200}
-
-result = zip(tuple1, set1)
-print(list(result))
+while i > 9:
+    while j > 2:
+        print("j")
+        j = j - 1
+    print(i)
+    i = i - 1
