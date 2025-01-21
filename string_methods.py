e5b7db6b6a437bfea429ee950964d04e52a6556e
gagan <gasingla07@gmail.com>
2025-01-21 14:59:37 -0600

    string methods

---
+++ string_methods.py
@@ -0,0 +1,19 @@
+# string methods
+
+# strip
+
+name = "   Rohit sharma   "
+
+print(name.strip())
+
+new_name = ".a..Rohit Sharma..b."
+
+print(new_name.strip('.aRmb'))
+
+
+message = "All the best for your GATE 2025 Exam"
+print(message.split())
+
+print("how are you doing today?".split(' ', 3))
+
+print(message.split(' ', maxsplit=2))
