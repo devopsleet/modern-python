908b7e40955fad439a99ca19ce0d1241a700c892
gagan <gasingla07@gmail.com>
2025-01-23 14:28:18 -0600

    tuple methods

--- list_methods.py
+++ list_methods.py
@@ -13,24 +13,48 @@ batting_order.insert(-2, "Tejas")
 print(batting_order)
 
 
-input_user = input("Enter the input ")
-print(type(input_user))
+# input_user = input("Enter the input ")
+# print(type(input_user))
+#
+#
+# # How to convert the user input to a list
+#
+# input_user_list_format = input_user.split()
+# print(input_user_list_format)
+# print(type(input_user_list_format))
 
 
-How to convert the user input to a list
+# new_string = "LiveAuctioners"
+# new_string[5:2] = "sss"
 
-input_user_list_format = input_user.split()
-print(input_user_list_format)
-print(type(input_user_list_format))
-
-
-new_string = "LiveAuctioners"
-new_string[5:2] = "sss"
-
-print(new_string)
+# print(new_string)
 
 
 new_list = ['a', 'b', 'c', 'd', 'e', 'f']
 print(new_list[4:2])
 new_list[4:2] = ['x', 'y', 'z']
 print(new_list)
+
+
+# list comprehensions
+
+n = []
+
+for i in range(1, 101):
+    n.append(i)
+
+
+l = [i for i in range(1, 101)]
+print(l)
+
+
+l = [i for i in range(1, 10) if i % 2 == 0]
+
+print(l)
+
+
+m = [1,2,3]
+n = [2,3,4]
+
+lc = [(i,j) if i + j > 3 else (j,i) for i in m for j in n]
+print(lc)
---
+++ tuple_methods.py
@@ -0,0 +1,9 @@
+t = ()
+print(t)
+
+t = tuple()
+print(t)
+
+
+t = 1
+print(type(t))
