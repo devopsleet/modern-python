10501b758e1f888cf2613cf02d932639b9e9bdf6
gagan <gasingla07@gmail.com>
2025-01-30 16:15:21 -0600

    class attributes

--- oop.py
+++ oop.py
@@ -1,9 +1,12 @@
 class Kettle(object):
 
+    power_source = "electricity" # class attribute
     def __init__(self, make, price):
         self.make = make
         self.price = price
         self.on = False
+    def switch_on(self):
+        self.on = True
 
 
 kenwood = Kettle("Kenwood", 8.99)
@@ -16,3 +19,25 @@ print(kenwood.price)
 hamilton = Kettle("Hamilton", 14.55)
 
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price,hamilton.make, hamilton.price))
+
+
+print("Models: {0.make} = {0.price}".format(kenwood))
+
+
+print(hamilton.on)
+hamilton.switch_on()
+print(hamilton.on)
+
+Kettle.switch_on(kenwood)
+
+
+print("*" * 80)
+
+# data attribute
+kenwood.power = 1.5
+print(kenwood.power)
+
+
+print(Kettle.power_source)
+print(kenwood.power_source)
+print(hamilton.power_source)
