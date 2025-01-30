e3f3183c730a26784ad3e36e4f3fc047aa430189
gagan <gasingla07@gmail.com>
2025-01-30 12:03:10 -0600

    OOPS introduction

--- oop.py
+++ oop.py
@@ -4,3 +4,15 @@ class Kettle(object):
         self.make = make
         self.price = price
         self.on = False
+
+
+kenwood = Kettle("Kenwood", 8.99)
+print(kenwood.make)
+print(kenwood.price)
+
+kenwood.price = 12.75
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton", 14.55)
+
+print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price,hamilton.make, hamilton.price))
