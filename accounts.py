df230f6b22599f4c1bfbdf31c6d737488c244164
gagan <gasingla07@gmail.com>
2025-02-20 16:39:45 -0600

    methods part 2 in OOPS

--- accounts.py
+++ accounts.py
@@ -1,29 +1,49 @@
+import datetime
+import pytz
+
+
 class Account:
-    "Simple account class with balance"
+    """Simple account class with balance"""
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
-
+        self.transaction_list = []
         print("Account created for " + self.name)
 
     def deposit(self, amount):
         if amount > 0 :
             self.balance += amount
+        self.show_balance()
+        self.transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
         if amount > 0:
             self.balance -= amount
-
+        self.show_balance()
+        self.transaction_list.append((Account._current_time(), -amount))
 
     def show_balance(self):
         print("Balance is {}".format(self.balance))
 
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                trans_type = "deposited"
+            else:
+                trans_type = "withdrawn"
+            print("{} {} on {} (local time was {})".format(amount, trans_type, date, date.astimezone()))
+
 
 if __name__ == "__main__":
     gagan = Account("Gagan", 0)
-    gagan.show_balance()
+    # gagan.show_balance()
     gagan.deposit(1000)
-    gagan.show_balance()
+    # gagan.show_balance()
     gagan.withdraw(500)
-    gagan.show_balance()
+    # gagan.show_balance()
+    gagan.show_transactions()
