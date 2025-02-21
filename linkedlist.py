0d65c0d3b810b56df44ee4672874f382c036605e
gagan <gasingla07@gmail.com>
2025-02-21 15:00:33 -0600

    Doubly linked list implementation with sentinel nodes

--- linkedlist.py
+++ linkedlist.py
@@ -1,19 +1,43 @@
 class ListNode:
+
     def __init__(self, val):
         self.val = val
+        self.prev = None
         self.next = None
 
+    @staticmethod
+    def add_to_start(node_to_add):
+        node_to_add.prev = head
+        node_to_add.next = head.next
+        head.next.prev = node_to_add
+        head.next = node_to_add
 
-one = ListNode(1)
-print(one)
+    @staticmethod
+    def display():
+        ptr = head
+        while ptr:
+            if ptr.next:
+                print(ptr.val, end="->")
+            else:
+                print(ptr.val)
+            ptr = ptr.next
+            # print(ptr.val, end="->")
+            # ptr = ptr.next
+            # if ptr == tail:
+            #     print(ptr.val)
+            #     ptr = ptr.next
 
-two = ListNode(2)
 
-three = ListNode(3)
+head = ListNode('head')
+tail = ListNode('tail')
+head.next = tail
+tail.prev = head
 
-one.next = two
-two.next = three
-head = one.next
+one = ListNode(20)
+two = ListNode(30)
+three = ListNode(40)
 
-print(head.val)
-print(head.next.val)
+ListNode.add_to_start(three)
+ListNode.add_to_start(two)
+ListNode.add_to_start(one)
+ListNode.display()
