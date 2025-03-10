e4c02978d5e79c4adacf613f11babd071f728c9e
gagan <gasingla07@gmail.com>
2025-03-09 20:51:19 -0500

    named variables

--- practice.py
+++ practice.py
@@ -1,21 +1,68 @@
-import collections
 
-str = "eat"
+import keyword
+# using semicolons
+# To have everything in a single line we can have a semicolon
+# semicolon acts as a separator
 
-# print(ord(str))
-print("".join(sorted(str)))
+print("abc");
+print("venky");
+
+# indentation
+# Python follows an indentation
+
+print("bac")
+print("venky")
+
+Welcome_message = "Welcome to Python class"
+
+# welcome message
+# variable name cannot have a space in between
+
+_abc = 9
+print(_abc)
+
+_ = 100
+print(_)
+
+# avoid using the python keyword
 
 
-chars = [1,0,1,0,0]
 
-print(tuple(chars))
+print(keyword.kwlist)
+
+# Dont use in built functon in Python
+
+print("ABC")
+
+#
+# print = 10
+#
+# print(print)
 
 
-print(ord('e') - ord('a'))
+# descriptive names
 
-dict = collections.defaultdict(list)
+name = "venky"
 
-dict[(1,0,0)].append('a')
-dict[(1,0,0)].append('b')
-print(list(dict.keys()))
-print(list(dict.values()))
+print(name)
+
+s_n = "abi"
+
+# prin the length of the variable
+
+len(name)
+
+name_length = "gagan"
+print(name_length)
+
+
+#print(name.len())
+
+
+#try to avoid using capital letters
+# use capital letters for constants
+NUM = 10
+num = 10
+
+print(NUM)
+print(num)
