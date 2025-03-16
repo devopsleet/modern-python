012c59743acc8de53985ad6d8ae84f2d0a290e8f
gagan <gasingla07@gmail.com>
2025-03-16 14:57:15 -0500

    add 3 numbers to get the sum equals to k

--- 3sum.py
+++ 3sum.py
@@ -1,39 +1,7 @@
-from typing import List
+s = "abcde"
 
-nums = [-1,0,1,2, -1, 4]
+length = len(s)
 
-
-def threeSum(nums: List[int], target: int) -> List[List[int]]:
-    def twoSum(i):
-        #lo = i - 1
-        mi = i
-        hi = len(nums) - 1
-
-        while mi < hi:
-            curr = nums[i-1] + nums[mi] + nums[hi]
-            if curr < 0:
-                mi = mi + 1
-            elif curr > 0:
-                hi = hi - 1
-            else:
-                ans.append([nums[i], nums[mi], nums[hi]])
-
-                mi = mi + 1
-                hi = hi - 1
-                while mi < hi and nums[mi] == nums[mi-1]:
-                    mi = mi + 1
-
-    ans = []
-
-    nums.sort()
-
-    if nums[0] > 0:
-        return []
-    print(nums)
-    for i in range(1,len(nums)):
-        if nums[i-1] != nums[i]:
-            twoSum(i)
-    print(ans)
-
-
-threeSum(nums, 0)
+for _ in range(length):
+    s = s[1:] + s[0]
+    print(s)
