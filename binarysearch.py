75f917bcba3b10d429d15d01d0f4ac9d458e01f7
gagan <gasingla07@gmail.com>
2025-03-22 17:16:17 -0500

    find a lower bound in a binary search

--- binarysearch.py
+++ binarysearch.py
@@ -1,8 +1,8 @@
 # Find an upper bound
 
 
-nums = [-7,-4, -1, 0,3,9,12]
-
+#nums = [-7,-4, -1, 0,3,9,12]
+nums = [9,9,9,9,9,9,9]
 target = 9
 
 # instead of finding the exact location of the target, look for the position to insert the target
@@ -28,4 +28,30 @@ def bs_upper_bound(nums, target) -> int:
         return -1
 
 res = bs_upper_bound(nums,target)
+print(res)
+
+# find a lower bound
+
+def bs_lower_bound(nums, target)-> int:
+    l = 0
+    r = len(nums)
+
+    while l < r:
+        mid = (l + r) // 2
+
+        if nums[mid] < target:
+            l = mid + 1
+        elif nums[mid] == target:
+            r = mid
+        else:
+            r = mid
+
+    return l
+
+    # if l < len(nums) and nums[l] == target:
+    #     return l
+    # else:
+    #     return -1
+
+res = bs_lower_bound(nums, target)
 print(res)
