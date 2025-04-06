d43438c6b1802f413b05e1113f67102db52d14f5
gagan <gasingla07@gmail.com>
2025-04-06 13:14:37 -0500

    loops

---
+++ loops_practice.py
@@ -0,0 +1,23 @@
+# Loops
+
+
+print(chr(97))
+
+a = 10
+b = 12
+c = 5
+
+# multivaribale assignment in a single line
+a,b,c = 10,12,[1,2,3]
+
+print(a)
+print(b)
+print(c)
+print(type(c))
+
+
+scores = [12,14,16,17,18]
+
+for index, value in enumerate(scores):
+    print(index)
+    print(value)
