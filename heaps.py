302c305f66abc6bd15a5ad11c683a868158b7e3b
gagan <gasingla07@gmail.com>
2025-04-20 20:17:46 -0500

    sliding window and heap implementation | approach 2

--- heaps.py
+++ heaps.py
@@ -29,6 +29,20 @@ def find_length(s):
 
     return ans
 
+
+# approach 2
+
+def find_length(nums, k):
+    # curr is the current sum of the window
+    left = curr = ans = 0
+    for right in range(len(nums)):
+        curr += nums[right]
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+        ans = max(ans, right - left + 1)
+
+    return ans
 # from heapq import *
 # from graphviz import Digraph
 # # import os
