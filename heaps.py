d0a6e0948b705d1f6058b6ab0d799b825654a5b6
gagan <gasingla07@gmail.com>
2025-04-20 20:16:56 -0500

    sliding window and heap implementation

--- heaps.py
+++ heaps.py
@@ -15,7 +15,19 @@ heapify(heap)
 while heap:
     print(heappop(heap))
 
+def find_length(s):
+    # curr is the current number of zeros in the window
+    left = curr = ans = 0
+    for right in range(len(s)):
+        if s[right] == "0":
+            curr += 1
+        while curr > 1:
+            if s[left] == "0":
+                curr -= 1
+            left += 1
+        ans = max(ans, right - left + 1)
 
+    return ans
 
 # from heapq import *
 # from graphviz import Digraph
