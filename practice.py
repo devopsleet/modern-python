d18397df9b9b45aeb5d30c580c5a3d817a727c24
gagan <gasingla07@gmail.com>
2025-04-20 20:19:09 -0500

    subarray sum less than k

--- practice.py
+++ practice.py
@@ -66,3 +66,22 @@ print(.1 + .2)
 
 print(Decimal(.1))
 print(a + b)
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+
+        ans = left = 0
+        curr = 1
+
+        for right in range(len(nums)):
+            curr *= nums[right]
+            while curr >= k:
+                curr //= nums[left]
+                left += 1
+
+            ans += right - left + 1
+
+        return ans
