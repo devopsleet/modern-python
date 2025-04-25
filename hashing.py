60c086090707d833e889f7bf5213fe38bf48fd50
gagan <gasingla07@gmail.com>
2025-04-25 16:50:42 -0500

    hashing and hashmaps in Python

--- hashing.py
+++ hashing.py
@@ -33,3 +33,17 @@ for key in keys:
 values = hash_map.values()
 for val in values:
     print(val)
+
+my_hash_map = {}
+
+my_hash_map[4] = 83
+print(my_hash_map[4]) # Prints 83
+
+print(4 in my_hash_map) # Prints True
+print(854 in my_hash_map) # Prints False
+
+my_hash_map[8] = 327
+my_hash_map[45] = 82523
+
+for key, val in my_hash_map.items():
+    print(f"{key}: {val}")
