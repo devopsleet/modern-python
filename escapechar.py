e3d28f9fba03ea85319bf9ab719a13d5f64bad69
gagan <gasingla07@gmail.com>
2025-05-15 11:12:12 -0500

    escape characters

--- escapechar.py
+++ escapechar.py
@@ -1,12 +1,17 @@
-splitString = "This string has been\n split over\n several\n lines"
+splitString = "This string has been\nsplit over\nseveral lines"
 print(splitString)
+
 tabbedString = "1\t2\t3\t4"
 print(tabbedString)
-#
-print('The pet shop owner said "No, no, \'e\'s he is resting".')
-print("The pet shop owner said \"No, no, 'e's he is resting\".")
-print("""The pet shop owner said \
-"No, no, 'e's he is resting".""")
-#
-print("C:\\Users\\tim\\notes.txt");
-print(r"C:\Users\tim\notes.txt");
+
+print('The pet shop owner said "No, no \'e\'s uh,.... he\'s resting".')
+print("The pet shop owner said \"No, no 'e's uh,.... he's resting\".")
+print("""The pet shop owner said "No, no 'e's uh, .... he's resting".""")
+
+anotherSplitString = """This string has been \
+split over \
+several \
+lines
+"""
+
+print(anotherSplitString)
