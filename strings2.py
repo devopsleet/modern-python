65ade04a15c3b7d857b949d33f34c66b1a21117f
gagan <gasingla07@gmail.com>
2025-05-18 12:56:05 -0500

    str slice

--- strings2.py
+++ strings2.py
@@ -8,3 +8,24 @@ print(parrot[3])
 print()
 
 print(parrot[13 -14])
+
+print(parrot[0:6]) # Norweg
+
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
+print(parrot[10:14])
+print(parrot[10:])
+print(parrot[:])
+
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+print(parrot[:])
+
+letters = "abcdefghijklmno"
+
+print(parrot[-4:2])
+print(parrot[-4:12])
