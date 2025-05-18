fc2f00df3d10d388de02dbc661fa8bb283812468
gagan <gasingla07@gmail.com>
2025-05-18 12:22:50 -0500

    str type with positive and negative indexes

--- operators.py
+++ operators.py
@@ -3,15 +3,10 @@ b = 3
 
 print(a + b)
 print(a - b)
-print(a/b)
 print(a * b)
-print(a//b)
-print(a%b)
+print(a / b)
 
-print()
+print(a // b)
 
-
-for i in range(a//b):
-    print(i)
-
-print(a + b - 3)
+print(a % b)
+print
--- strings2.py
+++ strings2.py
@@ -1,35 +1,10 @@
-
 parrot = "Norwegian Blue"
 
 print(parrot)
 
 print(parrot[3])
 
-print(parrot[-1])
-print(parrot[-14])
+# negative indexing
+print()
 
-print(parrot[0:6])
-print(parrot[3:5])
-print(parrot[:9])
-print(parrot[10:])
-print(parrot[:])
-print(parrot[:6] + parrot[6:])
-
-
-letters = "abscdefghijklmnopqrstuvwxyz"
-
-# Slice with Negative numbers
-print(parrot[-4:-2])
-print(parrot[-4:12])
-
-print(parrot[0:6:2])
-print(parrot[0:6:3])
-number = "9,227,343,56565656"
-separators = number[1:4]
-print(number[1::4])
-
-
-values = "".join(char if char not in separators else " " for char in number).split()
-print([int(val) for val in values])
-
-# negative step
+print(parrot[13 -14])
