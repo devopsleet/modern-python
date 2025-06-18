aae6b1d4fb797e2cc7cfe5b4c007366ebf885cad
gagan <gasingla07@gmail.com>
2025-06-18 13:19:11 -0500

    operators

--- operators.py
+++ operators.py
@@ -1,12 +1,36 @@
-a = 12
-b = 3
+# type casting
 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
+a = 7
+print(type(a))
+b = 3.0
+print(type(b))
+c = a+ b
+print(c)
+print(type(c))
 
-print(a // b)
 
-print(a % b)
-print
+# int()
+
+a = 8.5
+i_1 = int(8.5)
+print(i_1)
+# b = "1.5"
+i_2 = int(b)
+print(i_2)
+c = "8"
+
+# str()
+
+a = 10.5
+print(str(a))
+b = 10.5
+print(str(b))
+
+c = int(c)
+
+
+# arithmetic operator
+
+print( 2 ** 5)
+print(-7 // 2)
+print(4 % 2.2)
