158e5d00c08dbabbace20ee156b500296ec64f1f
gagan <gasingla07@gmail.com>
2025-07-14 16:11:50 -0500

    regex | basic patterns

--- regex.py
+++ regex.py
@@ -8,3 +8,13 @@ if match:
     print('found', match.group())
 else:
     print('did not find')
+
+
+print(re.findall(r'a', "cat"))
+print(re.findall(r".", "cat"))
+print(re.findall(r"\.", "gagan.singla"))
+
+print(re.findall(r"a.b", "a_b a|b a+bc a b"))
+print(re.findall(r"a.b", "a_b a1b a b"))
+
+print(re.findall(r'\W+', 'hello world_123!'))
