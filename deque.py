b4c2ca2238b40a0a911fa6f9159e7504bb5ef915
gagan <gasingla07@gmail.com>
2025-08-05 10:11:15 -0500

    deque operations

--- deque.py
+++ deque.py
@@ -1,47 +1,46 @@
 from collections import deque
 
 dq = deque()
-
 print(dq)
 
-# append operation
-
-dq.append(5)
-dq.append(10)
-
+dq.append(1)
 print(dq)
 
-# appendleft(x)
-
-dq.appendleft(4)
-#dq.appendleft(3)
-
+dq.append(2)
 print(dq)
 
 
-# pop operation
-# remove from the right end and returns the element
-
-popped = dq.pop()
-print(popped)
+dq.append(3)
 print(dq)
 
-# Extend operation
 
-dq.extend([30,40,50])
-dq.extend((70, 80))
-print(dq)
-
-# extendleft(iterables) elements are added from the left in reverse order
-
-dq.extendleft([1,2,3])
+dq.appendleft(-1)
+dq.appendleft(-2)
 
 print(dq)
 
-# rotate(n) - rotate the deque
 
-dq.rotate(2)
+dq.appendleft(-3)
 print(dq)
 
-dq.rotate(-2)
+
+dq.popleft()
+print(dq)
+
+
+list = [7,8,9]
+dq.extend(list)
+print(dq)
+
+dq.extendleft(list)
+print(dq)
+
+
+dq.reverse()
+
+print(dq)
+
+
+print(dq.count(9))
+dq.remove(8)
 print(dq)
--- lru_cache.py
+++ lru_cache.py
@@ -13,6 +13,11 @@ class LRUCache:
         self.tail = ListNode(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
+        self.capacity.head = ListNode(-1, -1)
+        self.capacity.tail = ListNode(-2, -3)
+        self.capacity.
+
+
 
 
     def get(self, key: int) -> int:
