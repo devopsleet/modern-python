dc6ae2062526cbff30d61bc8d3ee5a6e5c1b2c67
gagan <gasingla07@gmail.com>
2026-01-21 15:21:56 -0600

    calculate maximum depth of binary tree

--- practice.py
+++ practice.py
@@ -1,13 +1,52 @@
-from typing import List
+# Calculate the maximum depth of binary tree
 
-nums = [8,7,6,5,4,3,2,1]
+from typing import Optional
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-def selection_sort(nums: List[int]) -> None:
+
+root = TreeNode(0)
+one = TreeNode(1)
+Three = TreeNode(3)
+Five = TreeNode(5)
+Seven = TreeNode(7)
+
+root.left = one
+root.right = Three
+Three.left = Five
+Five.left = Seven
 
 
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
 
-    print(nums)
+        maxDepth = float('-inf')
 
+        def dfs(node, depth):
+            nonlocal maxDepth
 
-selection_sort(nums)
-print(nums)
+            if not node:
+                return
+            if not node.left and not node.right:
+                maxDepth = max(maxDepth, depth)
+
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+
+            return
+
+        dfs(root, 1)
+
+        return maxDepth
+
+s = Solution()
+maxDepth = s.maxDepth(root)
+print(maxDepth)
+
+# Time Complexity = O(n)
+# Space Complexity = O(n)
