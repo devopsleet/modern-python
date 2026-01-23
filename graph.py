aa7c90d42a88df25ce962c2ea63ed2e7769ac986
gagan <gasingla07@gmail.com>
2026-01-23 13:34:16 -0600

    calculate number of towns

---
+++ graph.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+from typing import List
+
+isConnected = [
+    [1,1,0],
+    [1,1,0],
+    [0,0,1]
+]
+
+def calculate_provinces(isConnected: List[List[int]])->int:
+
+    def dfs(node):
+        for neighbor in graph[node]:
+            if neighbor not in seen:
+                seen.add(neighbor)
+                dfs(neighbor)
+
+    # build the graph
+    n = len(isConnected)
+    graph = defaultdict(list)
+    for i in range(n):
+        for j in range(i+1,n):
+            if isConnected[i][j]:
+                graph[i].append(j)
+                graph[j].append(i)
+
+    # need a set to track the visited nodes
+    seen = set()
+
+    ans = 0
+
+    for i in range(n):
+        if i not in seen:
+            ans += 1
+            seen.add(i)
+            dfs(i)
+
+    return ans
+
+number_of_province = calculate_provinces(isConnected)
+print(number_of_province)
