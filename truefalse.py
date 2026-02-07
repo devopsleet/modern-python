d2b4c65a529c321f9ecd6d30afb5ef0db9af11f9
gagan <gasingla07@gmail.com>
2026-02-06 20:17:11 -0600

    add boolean expression

--- truefalse.py
+++ truefalse.py
@@ -1,10 +1,8 @@
-if 0:
-    print("True")
-else:
-    print("False")
+day = "Friday"
+temperature = 30
+raining = True
 
-name = input("Please enter your name: ")
-if name:
-    print("Hello {}".format(name))
+if (day == "Saturday" and temperature > 27) or (not raining):
+    print("Go swimming")
 else:
-    print(" No name")
+    print("Learn Python")
