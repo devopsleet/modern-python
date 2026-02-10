dbecb2103a54af6bd03b6db2ccfe8eab384ad719
gagan <gasingla07@gmail.com>
2026-02-10 15:39:22 -0600

    calculate provinces

--- graph.py
+++ graph.py
@@ -1,55 +1,38 @@
-from collections import defaultdict
+# Calculate the number of provinces
+import collections
 from typing import List
 
 isConnected = [
-    [1,1,0],
-    [1,1,0],
-    [0,0,1]
+     [1,1,0],
+     [1,1,0],
+     [0,0,1]
 ]
 
-def calculate_provinces(isConnected: List[List[int]])->int:
-
-    # stack implementation of a neighboring towns
+def calculate_provinces(isConected: List[List[int]])->int:
 
     def dfs(node):
-        stack = [node]
-
-        while stack:
-            node = stack.pop()
-
-            for neighbor in graph[node]:
-                if neighbor not in seen:
-                    seen.add(neighbor)
-                    stack.append(neighbor)
-        # for neighbor in graph[node]:
-        #     if neighbor not in seen:
-        #         seen.add(neighbor)
-        #         dfs(neighbor)
-
-    # build the graph
+        for neigbor in graph.get(node, []):
+            if neigbor not in seen:
+                seen.add(neigbor)
+                dfs(neigbor)
     n = len(isConnected)
-    graph = defaultdict(list)
+
+    graph = {}
+
     for i in range(n):
         for j in range(i+1,n):
             if isConnected[i][j]:
-                graph[i].append(j)
-                graph[j].append(i)
-                graph[]
+                graph.setdefault(i,[]).append(j)
+                graph.setdefault(j,[]).append(i)
 
-    # need a set to track the visited nodes
     seen = set()
-
-    ans = 0
-
+    ans =0
     for i in range(n):
         if i not in seen:
             ans += 1
             seen.add(i)
             dfs(i)
-
-
     return ans
 
-
-number_of_province = calculate_provinces(isConnected)
-print(number_of_province)
+answer = calculate_provinces(isConnected)
+print(answer)
