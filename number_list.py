5966307299b4956f2eb6367f2077d1c4eb96b877
gagan <gasingla07@gmail.com>
2026-02-22 18:43:48 -0600

    add implementation of sequence operations

--- number_list.py
+++ number_list.py
@@ -1,46 +1,16 @@
-empty_list = []  # create ane empty list
+even = [2,4,6,8]
+odd = [1,3,5,7,9]
+print(type(even))
+even.append(10)
+print(even)
+print(min(even))
+print(max(even))
 
-even = [2, 4, 6, 8, 10]
-odd = [1, 3, 5, 7, 9]
+print(min(odd))
+print(max(odd))
 
-# numbers = even + odd
-numbers = [even,odd]
-print(numbers)
+print(len(even))
+print(len(odd))
 
-for number_list in numbers:
-    print(number_list)
-
-    for value in number_list:
-        print(value)
-
-# sorted_numbers = sorted(numbers)
-# print(sorted_numbers)
-# print(numbers)
-#
-# digits =
-# #
-#
-# even.extend(odd)
-# print(even)
-# another_even = even
-# print(another_even)
-#
-# even.sort(reverse=True)
-# print(even)
-#
-# print(another_even)
-# print(min(even))
-# print(max(even))
-# print(min(odd))
-# print(max(odd))
-#
-#
-# print()
-#
-# print(len(even))  # number of items
-# print(len(odd))   # number of items
-#
-#
-# print()
-#
-# print("mississippi".count("s"))
+print()
+print("mississipi".count("issi"))
