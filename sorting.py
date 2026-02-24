4e051bbbbf80dd86465b70063e9ae86fe940193d
gagan <gasingla07@gmail.com>
2026-02-24 11:13:43 -0600

    add implementation of quickselect algorithm

--- sorting.py
+++ sorting.py
@@ -1,28 +1,22 @@
-# quick sort
+from collections import Counter
 
-nums = [1,5,3,2,8,7,6,4]
+# nums = [4,5,5,2,1,1,1,5,2,3,3,5,2]
+# k = 3
 
-def quick_sort(nums):
 
-    n = len(nums)
-    qsort(nums, 0, n-1)
+nums = [4,5,5,2,1,1,1,5,2,3,3,5,2]
 
-def qsort(nums,lo,hi):
 
-    if lo < hi:
-        p = partition(nums,lo,hi)
-        qsort(nums,lo,p-1)
-        qsort(nums,p+1, hi)
+count = Counter(nums)
 
-def partition(nums,lo,hi):
-    pivot = nums[hi]
-    i = lo
-    for j in range(lo,hi):
-        if nums[j] < pivot:
-            nums[i],nums[j] = nums[j], nums[i]
-            i+=1
-    nums[i],nums[hi] = nums[hi], nums[i]
-    return i
+print(count)
+unique = list(count.keys())
+print(unique)
+k = 3
 
-quick_sort(nums)
-print(nums)
+def quick_select(unique, k):
+
+    n = len(unique)
+
+    left = 0
+    right = n - 1
