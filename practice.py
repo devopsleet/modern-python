19e6144b28267d0d228b91d07fb41eafdda9a594
gagan <gasingla07@gmail.com>
2026-03-12 22:20:22 -0500

    add implementation of heap sort

--- practice.py
+++ practice.py
@@ -1,16 +1,31 @@
 class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        """
-        :type s: str
-        :rtype: int
-        """
-        # build hash map: character and how often it appears
-        count = collections.Counter(s)
+    def sortArray(self, nums: List[int]) -> List[int]:
 
-        # find the index
-        for idx, ch in enumerate(s):
-            if count[ch] == 1:
-                return idx
-        return -1
+        def heapify_top_down(n, i):
+            largest = i
 
-#print(new_arr)
+            lc = (2 * i) + 1
+            rc = (2 * i) + 2
+
+            if lc < n and nums[lc] > nums[largest]:
+                largest = lc
+            if rc < n and nums[rc] > nums[largest]:
+                largest = rc
+
+            if largest != i:
+                nums[i], nums[largest] = nums[largest], nums[i]
+                heapify_top_down(n, largest)
+
+        def  heap_sort():
+
+            n = len(nums)
+
+            for i in range(n//2-1, -1, -1):
+                heapify_top_down(n, i)
+
+            for i in range(n-1,0,-1):
+                nums[i], nums[0] = nums[0], nums[i]
+                heapify_top_down(i,0)
+
+        heap_sort()
+        return nums
