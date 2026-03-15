6696caccb310619cad4af83b8cba26c322694c5f
gagan <gasingla07@gmail.com>
2026-03-15 18:10:15 -0500

    add implementation of quicksort

--- sorting.py
+++ sorting.py
@@ -1,44 +1,25 @@
-# merge sort
+# Quick Sort
 
 from typing import List
 
-#nums = [5,1,1,2,0,0]
+nums = [5,1,1,2,0,0]
 
-def mergeSort(nums: List[int], p: int, r: int)-> List[int]:
-
+def quickSort(nums: List[int], p: int, r: int):
     if p < r:
-        q = (p+r)//2
-        mergeSort(nums, p,q)
-        mergeSort(nums,q+1,r)
-        merge(nums,p,q,r)
+        q = partition(nums,p,r)
+        quickSort(nums,p,q-1)
+        quickSort(nums,q+1,r)
 
-def merge(nums,p,q,r):
-    n1 = q-p + 1
-    n2 = r-q
+def partition(nums: List[int], p:int, r:int):
+    pivot = nums[r]
 
-    list1 = [0] * (n1+1)
-
-    for i in range(n1):
-        list1[i] = nums[p+i]
-
-    list1[-1] = float('inf')
-
-    list2 = [0] * (n2+1)
-
-    for i in range(n2):
-        list2[i] = nums[q+i+1]
-
-    list2[-1] = float('inf')
-
-    i = j = 0
-
-    for k in range(p,r+1):
-        if list1[i] <= list2[j]:
-            nums[k] = list1[i]
+    i = p-1
+    for j in range(p,r):
+        if nums[j] <= pivot:
             i += 1
-        else:
-            nums[k] = list2[j]
-            j += 1
+            nums[i], nums[j] = nums[j], nums[i]
+    nums[i+1], nums[r] = nums[r], nums[i+1]
+    return i+1
 
-mergeSort(nums,0, len(nums)-1)
+quickSort(nums, 0, len(nums) -1)
 print(nums)
