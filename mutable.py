7c7df78a6e059e37fc35b0e96425326e8994a299
gagan <gasingla07@gmail.com>
2026-04-23 20:27:11 -0500

    count method for sequence types

--- mutable.py
+++ mutable.py
@@ -1,22 +1,20 @@
 shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 
-another_list = shopping_list
 
+another_list = shopping_list
 print(id(shopping_list))
 print(id(another_list))
 
 shopping_list += ["cookies"]
-
 print(shopping_list)
 print(id(shopping_list))
 print(id(another_list))
 
-
 print(another_list)
 
-a = b = c = d = f = another_list
+a = b = c = d = e = f = another_list
 print(a)
-print("adding cream")
+print("Adding cream")
 b.append("cream")
 print(c)
 print(d)
--- number_list.py
+++ number_list.py
@@ -1,31 +1,14 @@
-empty_list= []
 even = [2,4,6,8]
 odd = [1,3,5,7,9]
 
-numbers = even + odd
-
-print(numbers)
-# = list(1,2,3)
-third = tuple((3,4,5))
-even.extend(odd)
-even.extend(third)
-
-print(even)
-another_even = even
-print(another_even)
-
-even.sort(reverse=True)
-print(even)
-print(another_even)
-print(id(even))
-print(id(another_even))
+print(min(even))
+print(max(even))
+print(min(odd))
+print(max(odd))
 
 
-arr = [[1,2], [3,4]]
+print()
+print(len(even))
+print(len(odd))
 
-new_arr = arr.copy()
-
-arr[0].append(5)
-
-print(arr)
-print(new_arr)
+print("mississippi".count('iss'))
