19c71561aa65f80192b05bf1e25d065367e2d6f6
gagan <gasingla07@gmail.com>
2026-05-04 19:08:39 -0500

    outliers in a data set

--- listsIntro.py
+++ listsIntro.py
@@ -4,10 +4,5 @@ computer_parts = ["computer",
                   "mouse",
                   "mouse mat"
                   ]
-for part in computer_parts:
-    print(part)
-
-print()
-print(computer_parts[2])
-print(computer_parts[0:3])
-print(computer_parts[-1])
+computer_parts[3:] = ("trackball",)
+print(computer_parts)
