29ae788fa6bcce5bdec9f22674c7020f3e1a84a0
gagan <gasingla07@gmail.com>
2026-05-09 08:09:01 -0500

    use PEP 8 style guide

--- number_list.py
+++ number_list.py
@@ -5,40 +5,11 @@ empty_list = []
 even = [2,4,6,8]
 odd = [1,3,5,7,9]
 
-numbers = even + odd
+numbers = [even, odd]
 print(numbers)
 
+for number_list in numbers:
+    print(number_list)
 
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
-
-print(numbers)
-
-
-digits = list("432985617")
-print(digits)
-
-
-# more_numbers = list(numbers)
-# print(more_numbers)
-#
-# print(numbers is more_numbers)
-# print(numbers == more_numbers)
-
-# more_numbers = numbers[:]
-# print(more_numbers is numbers)
-# more_numbers.append(10)
-# print(f"More numbers is {more_numbers}")
-# print(f"Original list is {numbers}")
-
-l1 = [[1,2], [3,4]]
-# l2 = l1[:]
-# print(l1 is l2)
-
-# l1[0].append(5)
-l2 = copy.deepcopy(l1)
-print(l2)
-l2.append(10)
-print(l1)
-print(l2)
-print(l1 is l2)
+    for value in number_list:
+        print(value)
--- spam.py
+++ spam.py
@@ -8,36 +8,14 @@ menu = [
     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
     ["spam", "egg", "spam", "spam", "bacon", "spam"],
 ]
-# for meal in menu:
-#     if "spam" not in meal:
-#         print(meal)
-
 
 for meal in menu:
-    for index in range(len(meal) - 1, -1, -1):
-        if meal[index] == "spam":
-            del meal[index]
-    print(", ".join(meal))
+    if "spam" not in meal:
+        print(meal)
 
-# for meal in menu:
-#     if "spam" not in meal:
-#         print(meal)
-#
-#         for item in meal:
-#             print(item)
-#     else:
-#         print("{0} has a spam score of {1}".format(meal, meal.count("spam")))
+        for item in meal:
+            print(item)
 
-
-# for meal in menu:
-#     for index in range(len(meal) - 1, -1, -1):
-#         if meal[index] == "spam":
-#             del meal[index]
-#     print(", ".join(meal))
-#
-# #
-# for meal in menu:
-#     for item in meal:
-#         if item != "spam":
-#             print(item)
-#     print()
+    else:
+        print("{0} has a spam score of {1} "
+              .format(meal, meal.count("spam")))
