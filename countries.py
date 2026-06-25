d8688e27f3aaf5b7f65ac65743bc9eec480680bb
gagan <gasingla07@gmail.com>
2026-06-25 10:49:05 -0500

    read csv file

--- countries.py
+++ countries.py
@@ -1,43 +1,44 @@
-filename = 'D:/Python/country_info.txt'
+input_file = 'country_info.txt'
 
 countries = {}
-with open(filename) as country_file:
+
+with open(input_file) as country_file:
+    # print(repr(country_file.readline()))
     country_file.readline()
+
     for row in country_file:
-        data = row.strip('\n').split('|')
+        data = row.rstrip('\n').split('|')
         country, capital, code, code3, dialing, timezone, currency = data
-        # print(country, capital, code, code3, dialing, timezone, currency, sep="\n\t")
+
         country_dict = {
-            'name': country,
-            'capital': capital,
-            'country_code': code,
-            'cc3': code3,
-            'dialing_code': dialing,
-            'timezone': timezone,
-            'currency': currency
+            'name' : country,
+            'capital' : capital,
+            'country_code' : code,
+            'cc3' : code3,
+            'dialing_code' : dialing,
+            'timezone' : timezone,
+            'currency' : currency
+
         }
-        print(country_dict)
+
+        # print(country_dict)
         countries[country.casefold()] = country_dict
-        countries[code.casefold()] = country_dict
+#
+# print(countries)
+get_name = input("Please Enter the country name = ")
 
-
+if get_name in countries:
+    #print(get_name.casefold())
+    data = countries[get_name]
+    print(data['capital'])
 # print(countries)
 
-for country, data in countries.items(0):
-    print(country)
-
+# countries = ["a", "b", "c", "d"]
+#
+# it = iter(countries)
+#
 # while True:
-#     chosen_country = input("Please select the country name ").casefold()
-#     if chosen_country in countries:
-#         country_data = countries[chosen_country]
-#         print(f"The capital city is {country_data['capital']}")
-#     elif chosen_country == 'quit':
+#     try:
+#         print(next(it))
+#     except StopIteration:
 #         break
-
-# while True:
-#     country_name_entered_by_user = input("Enter the country name = ")
-#     for country, data in countries.items():
-#         if country_name_entered_by_user in countries:
-#             print(f"The capital city of {country_name_entered_by_user} is {countries['country['capital']']}")
-#         else:
-#             break
